7fb7e868796dbe6357958895b556090782857809
AthitiJames <james@odds.team>
2024-09-06 13:06:21 +0700

    add route edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update, :index]
 
   get '/register', to: 'users#new'
 end
